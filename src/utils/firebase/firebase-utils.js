06bb8de5d627652c5a36a401277233ec5a2e6bbb
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-04-17 17:10:58 -0400

    Store users to firebase storage

--- src/utils/firebase/firebase-utils.js
+++ src/utils/firebase/firebase-utils.js
@@ -37,5 +37,27 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot.exists());
+  // console.log(userSnapshot.exists());
+
+  // if user data does not exist
+  // create / set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  // check if user data exists
+
+  return userDocRef;
+  // return userDocRef
 };
